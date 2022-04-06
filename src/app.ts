c5a459d6a2d95c8a36cbd8b5fb3eb0e5f4738b88
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-06 18:51:11 +0100

    ,

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,11 @@ import express from "express"
 
 const app = express()
 const backUpPort = 6900
+
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
+
+app.listen(backUpPort || process.env.PORT, () => {
+  console.log(`Server is running on port ${backUpPort || process.env.PORT}`)
+})
