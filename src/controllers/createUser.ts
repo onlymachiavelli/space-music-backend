125b54e024fcc8ef23a95a811bc71c5c22c7eb48
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-10 04:04:28 +0100

    bodyparser mod + stffs

--- src/controllers/createUser.ts
+++ src/controllers/createUser.ts
@@ -1,19 +1,9 @@
 import UserTasks from "../services/userServ"
 import { RequestHandler } from "express"
 const createUser: RequestHandler = async (req, res) => {
-  const data = req.body || {
-    fullname: "Alaa Barka",
-    email: "tmakaveli643@gmail.com",
-    phoneNumber: "54324884",
-    password: "hello world",
-    musicList: {
-      total: 0,
-      list: [],
-    },
-  }
   try {
-    await UserTasks.addUser(data)
-    return res.status(201).send("User created")
+    await UserTasks.addUser(req.body)
+    return res.status(201).send("User created" + req.body)
   } catch (e) {
     return res.status(500).send("Error" + e)
   }
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 const app = express()
 app.use(cors())
-app.use("/hello", userRoute)
+app.use("/user", userRoute)
 
 connect()
   .then(() => {
