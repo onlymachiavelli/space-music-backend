6283cda62f9e7572be9df189d8d9de509cffb9e3
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-10 05:14:01 +0100

    fcked up

--- src/controllers/createUser.ts
+++ src/controllers/createUser.ts
@@ -1,13 +1,30 @@
 import UserTasks from "../services/userServ"
 import { RequestHandler } from "express"
+
+import axios from "axios"
 //importing body parser
+const hostname = "localhost"
+
 const createUser: RequestHandler = async (req, res) => {
-  const data = req.body
-  try {
-    await UserTasks.addUser(data)
-    return res.status(201).send("User created")
-  } catch (e) {
-    return res.status(500).send("Error" + e)
+  let result: boolean = false
+
+  const username = req.body.username
+  const find = await axios
+    .get(`${hostname}:6900/user/${username}`)
+    .then((response) => {
+      result = response.data !== null ? false : true
+    })
+
+  if (result) {
+    const data = req.body
+    try {
+      await UserTasks.addUser(data)
+      return res.status(201).send("User created")
+    } catch (e) {
+      return res.status(500).send("Error" + e)
+    }
+  } else {
+    res.status(500).send("user already exists")
   }
 }
 
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,17 +2,14 @@ import express from "express"
 import createUser from "../controllers/createUser"
 import deleteUser from "../controllers/deleteUser"
 import getUser from "../controllers/findUser"
-//importing bodyparser
 import bodyParser from "body-parser"
 const userRoute = express.Router()
-
-userRoute.post("/", bodyParser.json(), () => {
-  if (true) {
-    createUser.createUser
-  }
-})
 
 userRoute.delete("/:id", deleteUser.deleteUser)
 
 userRoute.get("/:id", getUser.finduser)
 export default userRoute
+
+userRoute.post("/", bodyParser.json(), () => {
+  createUser.createUser
+})
