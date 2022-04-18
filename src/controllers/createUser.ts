17d867091766f1f3492f4749aa90f27fee1e124f
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-18 01:34:08 +0100

    salt & pepper

--- src/controllers/createUser.ts
+++ src/controllers/createUser.ts
@@ -2,17 +2,24 @@ import * as useTasks from "../services/userServ"
 import express, { RequestHandler } from "express"
 import bcrypt from "bcrypt"
 const createUser: RequestHandler = async (req, res) => {
-  const id = "onlymachiavelli"
+  const id = req.body._id
   const user = await useTasks.getUser(id)
-  console.log(user)
+
   if (user !== null) {
     res.status(400).send("User already exists")
   } else {
     let data = req.body
-    try {
-      data.password = await bcrypt.hash(data.password, process.env.SALT || "")
 
+    const hash = await bcrypt.hash(
+      data.password,
+      Number(process.env.SALT) || 10
+    )
+    data.password = hash
+
+    try {
+      console.log(data)
       await useTasks.addUser(data)
+
       return res.status(201).send("User created")
     } catch (e) {
       //return res.status(500).send("Error" + e)
