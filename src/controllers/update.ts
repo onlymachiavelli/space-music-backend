88e78f8d7694aa4f48b57399ddb0c66df08de4eb
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-20 11:40:10 +0100

    setting up the update file

--- src/controllers/update.ts
+++ src/controllers/update.ts
@@ -2,6 +2,18 @@ import * as UserTasks from "./../services/userServ"
 
 import express, { RequestHandler } from "express"
 
-const updateField: RequestHandler = async (req, res) => {}
+const updateField: RequestHandler = async (req, res) => {
+  try {
+    const id = "onlymachiavelli"
+    const data = {
+      fullname: "sidek alaa",
+    }
+
+    await UserTasks.update(id, data)
+    console.log("updated")
+  } catch (e) {
+    res.status(500).send("Error" + e)
+  }
+}
 
 export default { updateField }
